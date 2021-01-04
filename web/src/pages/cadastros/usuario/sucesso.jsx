8f4850cf3be315654bff5cd844e233b30e999c53
Otávio Guimarães Silva <otavio.guimaraes.silva@gmail.com>
2021-01-04 08:45:16 -0300

    Voltado
    Voltado

---
+++ web/src/pages/cadastros/usuario/sucesso.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import carregando from '../../../img/carregando.gif'
+
+
+const Sucesso = ({history}) => {
+    
+    function redirect () {
+        setTimeout(function () {
+            history.push('/')   
+        }, 3000);   
+    }
+    
+    window.onload = redirect()
+
+    return (
+
+        <div className="center">
+            <h1>Cadastro realizado com sucesso</h1>
+            <h2>Você será redirecionado para a tela de Login</h2>
+            <img alt="carregando" src={carregando} width="40"></img>
+        </div>
+    );
+};
+
+export default Sucesso;
